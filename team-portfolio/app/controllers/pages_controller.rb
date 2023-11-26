f9758350e3467b7a3aa0fa4957dfbaec5099eb7b
KaiLindholm <kailindholm20022@gmail.com>
2023-11-26 17:34:05 -0600

    front page setup. Individual routes created

--- team-portfolio/app/controllers/pages_controller.rb
+++ team-portfolio/app/controllers/pages_controller.rb
@@ -2,4 +2,15 @@ class PagesController < ApplicationController
     def index
     end 
 
+    def kai
+    end
+
+    def james
+    end
+
+    def joe
+    end
+
+    def gustav 
+    end
 end
--- team-portfolio/config/routes.rb
+++ team-portfolio/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
     root :to => 'pages#index'
+
+    get 'kai' => 'pages#kai'
+    get 'james' => 'pages#james'
+    get 'joe' => 'pages#joe'
+    get 'gustav' => 'pages#gustav'
+    
 end
