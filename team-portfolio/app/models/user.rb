8596fe561cbe0e130928a0a0e9a12b8e9f83e268
James Ostrowski <jamesostrowski21@gmail.com>
2023-11-26 19:31:09 -0600

    moving files over

---
+++ team-portfolio/app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+    has_secure_password
+
+    validates :name, presence: true, length: {maximum: 25}
+    VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, format: {with: VALID_EMAIL}, uniqueness: {case_sensitive: false}
+    validates :password, presence: true, length: {minimum: 6}
+    validates :password_confirmation, presence: true
+    
+end
