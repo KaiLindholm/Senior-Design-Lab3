c5c8f45fff6da0da8a88902f87582fd7047c5491
KaiLindholm <kailindholm20022@gmail.com>
2023-11-30 21:51:39 -0600

    my page is finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,5 @@ Project.create(name: 'IoT Temperature System', repo: 'https://github.com/KaiLind
 Project.create(name: 'RF Transmitter', repo: '', description: 'An RF transmitter to determine if the area between it and the receiver are being blocked')
 Project.create(name: 'Team Portfolio', repo: 'https://github.com/KaiLindholm/Senior-Design-Lab3', description: 'A portfolio for the team to show off their work')
 Project.create(name: 'Capstone Project', repo: '', description: 'A satellite receiver that can be used to receive weather data from the GOES satellite')
+
+User.create(name: 'ECE4880', email: 'ECE4880@default.com' password: '2023FallLab3!')
